9ce42c542b80972c777d6519098b8e1e26ec0196
Jinsol Kwak <jkwak@c5r3s6.42seoul.kr>
2022-03-24 21:32:50 +0900

    draft

---
+++ ft_printf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkwak <jkwak@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/24 16:11:18 by jkwak             #+#    #+#             */
+/*   Updated: 2022/03/24 16:42:07 by jkwak            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef	FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdarg.h>
+
+int	ft_printf(const char *format, ...);
+
+#endif
